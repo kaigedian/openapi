0610dce7959f224a175f7228e830580ceb474438
huangzhuo <157788394@qq.com>
2019-05-07 00:07:39 +0800

    终于解决

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  title: '开个店开放平台',
+  description: '开个店，每个品牌都有自己的平台',
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png', type: 'image/x-icon' }]
+  ],
+  themeConfig: {
+    nav: [
+      {
+        text: '接口文档',
+        items: [
+          {
+            text: '接入必读',
+            link: '/guide/general'
+          },
+          {
+            text: '门店类接口',
+            link: '/guide/store'
+          },
+          {
+            text: '订单类API',
+            link: '/guide/order'
+          }
+        ]
+      },
+      {
+        text: '开个店商户后台',
+        link: 'https://micro.sandload.cn'
+      }
+    ],
+    sidebar: [
+      '/guide/general',
+      '/guide/store',
+      '/guide/order'
+    ],
+    // displayAllHeaders: true,
+    lastUpdated: 'Last Updated'
+  },
+  markdown: {
+    lineNumbers: true
+  },
+  plugins: [
+    '@vuepress/active-header-links',
+    '@vuepress/back-to-top',
+    '@vuepress/last-updated',
+    '@vuepress/medium-zoom',
+    '@vuepress/nprogress'
+  ]
+}
